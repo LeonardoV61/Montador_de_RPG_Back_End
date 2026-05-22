2b49e4605c5849e06e5fbf9c55013047d8a4881e
ErikPalazziDev <erik071213@gmail.com>
2026-05-22 00:52:59 -0300

    Fixed issues and entities fully working now

--- src/main/java/com/rpgvtt/montador_de_rpg_backend/domain/model/campanha/Campanha.java
+++ src/main/java/com/rpgvtt/montador_de_rpg_backend/domain/model/campanha/Campanha.java
@@ -59,5 +59,5 @@ public class Campanha {
     private List<Personagem> personagens;
 
     @OneToMany(mappedBy = "campanha")
-    private List<Usuario> usuarios;
+    private List<CampanhaUsuario> usuarios;
 }
